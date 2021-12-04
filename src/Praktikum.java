0320408e3f0346ad684427bf87e926dd641337c6
Oleg Stolyarov <oastolyarov@gmail.com>
2021-12-04 18:38:51 +0300

    Убрал дублирующиеся импорты

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Scanner;
 
 public class Praktikum {
     public static void main(String[] args) {
